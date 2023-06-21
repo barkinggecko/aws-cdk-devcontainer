3de25763aa26455c362e6077a58ba8cde48fb889
Cristian Southall <cristian.southall@journeyone.com.au>
2023-06-21 00:46:56 +0000

    Build now forced clean (i.e. no cache).

--- .devcontainer/publish.sh
+++ .devcontainer/publish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker login -u barkinggecko
-docker build --tag barkinggecko/aws-cdk-devcontainer:latest .
+docker build --pull --no-cache --tag barkinggecko/aws-cdk-devcontainer:latest .
 docker push barkinggecko/aws-cdk-devcontainer:latest
